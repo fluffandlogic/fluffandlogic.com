98ed6a8cd5c23148d253cb6e3564c4e75dda5764
fluffandlogic <93094940+fluffandlogic@users.noreply.github.com>
2025-07-28 20:37:19 -0500

    Dynamically loading components
    Dynamically loading pages and javascript.

--- test/main.js
+++ test/main.js
@@ -1,36 +1,59 @@
+"use strict"
 gsap.registerPlugin(
     SplitText,
     ScrollTrigger,
 );
 
-let split = SplitText.create("h1", { type: "lines", mask: "lines" });
-gsap.from(split.lines, {
-    duration: 1,
-    y: 100,
-    autoAlpha: 0,
-    stagger: 0.1,
-});
+window.onload = ()=> {
+    load('/test/nav.html', '.main-nav');
+    load('/test/footer.html', '.main-footer');
+    load('/test/home.html', '.main-content', anim);
+};
 
-let imageContainers = document.querySelectorAll('.image');
+function anim() {
+    let split = SplitText.create("h2", { type: "lines", mask: "lines" });
+    gsap.from(split.lines, {
+        duration: 1,
+        y: 100,
+        autoAlpha: 0,
+        stagger: 0.1,
+    });
 
-imageContainers.forEach(function(imageContainer, index) {
-    let image = imageContainer.querySelector('img');
-    gsap.fromTo(image,
-        {
-            yPercent: -20
-        },
-        {
-            yPercent: 10,
-            ease: "none",
-            scrollTrigger: {
-                trigger: imageContainer,
-                scrub: true,
-                start: "top bottom",
-                end: "bottom top"
+    let imageContainers = document.querySelectorAll('.image');
+
+    imageContainers.forEach(function(imageContainer, index) {
+        let image = imageContainer.querySelector('img');
+        gsap.fromTo(image,
+            {
+                yPercent: -20
+            },
+            {
+                yPercent: 10,
+                ease: "none",
+                scrollTrigger: {
+                    trigger: imageContainer,
+                    scrub: true,
+                    start: "top bottom",
+                    end: "bottom top"
+                }
             }
-        }
-    );
-})
-let output = document.querySelector('.output');
-output.innerHTML = "Output = " + typeof imageContainer;
+        );
+    });
+}
+
+function load(filename, destination, code) {
+    fetch(filename)
+        .then(response => {
+            if (!response.ok) throw new Error('Could not load file');
+            return response.text();
+        })
+        .then(data => {
+            document.querySelector(destination).innerHTML = data;
+            if (code) code();
+
+        })
+        .catch(error=> {
+            console.error('Error loading the content: ', error);
+        })
+}
 
